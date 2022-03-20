2d34c973a8fc381a9b92ec3553d958c17f4169f2
Ravi Damarla <ravi.damarla@gmail.com>
2022-03-20 12:46:48 +0000

    added implementation

--- factory-sensors/factory-sensors.js
+++ factory-sensors/factory-sensors.js
@@ -16,7 +16,9 @@ export class OverheatingError extends Error {
  * @throws {Error}
  */
 export function checkHumidityLevel(humidityPercentage) {
-  throw new Error('Implement the checkHumidity function');
+  if (humidityPercentage > 70) {
+    throw new Error('The Humidity level of the production room is too high!');
+  }
 }
 
 /**
@@ -26,7 +28,14 @@ export function checkHumidityLevel(humidityPercentage) {
  * @throws {ArgumentError|OverheatingError}
  */
 export function reportOverheating(temperature) {
-  throw new Error('Implement the reportOverheating function');
+  if (temperature) {
+    if (temperature > 500) {
+      throw new OverheatingError(temperature);
+    }
+  } else {
+    // The sensor is broken.
+    throw new ArgumentError();
+  }
 }
 
 /**
@@ -41,5 +50,19 @@ export function reportOverheating(temperature) {
  * @throws {ArgumentError|OverheatingError|Error}
  */
 export function monitorTheMachine(actions) {
-  throw new Error('Implement the monitorTheMachine function');
+  try {
+    actions.check();
+  } catch (error) {
+    if (error instanceof ArgumentError) {
+      actions.alertDeadSensor();
+    } else if (error instanceof OverheatingError) {
+      if (error.temperature < 600) {
+        actions.alertOverheating();
+      } else {
+        actions.shutdown();
+      }
+    } else {
+      throw error;
+    }
+  }
 }
